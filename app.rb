330b8c0f8a6fdad243f49fc2ae7779c9d62a6058
David Le <43181047+DavidVLe1@users.noreply.github.com>
2023-11-25 03:35:32 +0000

    added rock, paper, scissor, home route.

--- app.rb
+++ app.rb
@@ -1,9 +1,24 @@
 require "sinatra"
 require "sinatra/reloader"
 
+cpu_choice=["rock","paper","scissors"]
+
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  erb(:home)
+end
+
+get("/rock") do
+
+  @cpu_random_choice=cpu_choice.sample
+  erb(:rock)
+end
+
+get("/paper") do
+  @cpu_random_choice=cpu_choice.sample
+  erb(:paper)
+end
+
+get("/scissors") do
+  @cpu_random_choice=cpu_choice.sample
+  erb(:scissors)
 end
